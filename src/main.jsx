c2d582ed6ca4a9f6cfaa9963d15c62d42c829162
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2024-11-22 03:42:30 +0800

    setup project

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@material-tailwind/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/global.css";
+import App from "@/App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
