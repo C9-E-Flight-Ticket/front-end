a23af216c5ce849fed3675e34eb1563f5b90bd9d
RifqiAfandi <rifqitriafandi.20@gmail.com>
2024-12-03 20:52:14 +0700

    feat: add  button departure and return in seat selection

--- src/features/transaction/components/SeatSelection.jsx
+++ src/features/transaction/components/SeatSelection.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
 
 const SeatSelection = () => {
   const { seatClass } = useSelector((state) => state.homepage);
-  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [activeTrip, setActiveTrip] = useState("Pergi");
+  const [selectedSeatsPergi, setSelectedSeatsPergi] = useState([]);
+  const [selectedSeatsPulang, setSelectedSeatsPulang] = useState([]);
 
   const getSeatLayout = (seatClass) => {
     switch (seatClass) {
@@ -27,13 +36,15 @@ const SeatSelection = () => {
   };
 
   const layout = getSeatLayout(seatClass);
-  const { leftRows, rightRows, cols } = layout;
   const totalSeats =
     (layout.leftRows.length + layout.rightRows.length) * layout.cols;
-  const seatRows = layout.leftRows.length;
-  const unavailableSeats = [];
+  const { leftRows, rightRows, cols } = layout;
 
-  const toggleSeat = (seat) => {
+  const toggleSeat = (seat, isPergi = true) => {
+    const setSelectedSeats = isPergi
+      ? setSelectedSeatsPergi
+      : setSelectedSeatsPulang;
+
     setSelectedSeats((prev) => {
       if (prev.find((s) => s.seat === seat)) {
         return prev.filter((s) => s.seat !== seat);
@@ -43,138 +54,152 @@ const SeatSelection = () => {
     });
   };
 
-  const getSeatLabel = (seat) => {
+  const getSeatLabel = (seat, isPergi = true) => {
+    const selectedSeats = isPergi ? selectedSeatsPergi : selectedSeatsPulang;
     const selected = selectedSeats.find((s) => s.seat === seat);
     return selected ? selected.label : null;
   };
 
-  return (
-    <div className="w-full p-5 mx-auto">
-      <div className="p-5 border border-textGrey">
-        <h1 className="mb-2 text-xl font-semibold">Pilih Kursi</h1>
-        <div className="p-3 text-center rounded-md bg-lightGreen">
-          <p className="text-xs font-normal text-white">
-            {seatClass} - {totalSeats} Seats available
-          </p>
-        </div>
-        <div className="gap-4 flex justify-center">
-          <div>
-            <div
-              className={`grid ${
-                seatRows === 3
-                  ? "grid-cols-3"
-                  : seatRows === 2
-                  ? "grid-cols-2"
-                  : "grid-cols-1"
-              } gap-x-4 text-textGrey`}
-            >
-              {leftRows.map((row) => (
-                <div
-                  key={row}
-                  className="w-12 h-12 flex items-center justify-center font-bold"
-                >
-                  {row}
-                </div>
-              ))}
-            </div>
-            <div
-              className={`grid ${
-                seatRows === 3
-                  ? "grid-cols-3"
-                  : seatRows === 2
-                  ? "grid-cols-2"
-                  : "grid-cols-1"
-              } gap-x-4 gap-y-3`}
-            >
-              {Array.from({ length: cols }, (_, i) => i + 1).map((col) =>
-                leftRows.map((row) => {
-                  const seat = `${row}${col}`;
-                  const isAvailable = !unavailableSeats.includes(seat);
-                  const label = getSeatLabel(seat);
-                  return (
-                    <button
-                      key={seat}
-                      onClick={() => isAvailable && toggleSeat(seat)}
-                      className={`w-12 h-12 flex items-center justify-center border rounded ${
-                        !isAvailable
-                          ? "bg-borderGrey cursor-not-allowed text-white"
-                          : label
-                          ? "bg-textPurple text-white"
-                          : "bg-lightGreen"
-                      }`}
-                      disabled={!isAvailable}
-                    >
-                      {isAvailable ? label || "" : "X"}
-                    </button>
-                  );
-                })
-              )}
-            </div>
-          </div>
-          <div className="gap-y-3 pt-12 flex flex-col items-center justify-center">
-            {Array.from({ length: layout.cols }, (_, i) => i + 1).map((col) => (
+  const renderSeatMap = (isPergi = true) => {
+    const unavailableSeats = [];
+
+    return (
+      <div className="flex justify-center gap-4">
+        <div>
+          <div
+            className={`grid grid-cols-${leftRows.length} gap-x-4 text-gray-600`}
+          >
+            {leftRows.map((row) => (
               <div
-                key={col}
-                className="w-6 h-12 flex items-center justify-center font-medium border rounded-lg text-textGrey bg-lightGray"
+                key={row}
+                className="flex items-center justify-center w-12 h-12 font-bold"
               >
-                {col}
+                {row}
               </div>
             ))}
           </div>
-          <div>
-            <div
-              className={`grid ${
-                seatRows === 3
-                  ? "grid-cols-3"
-                  : seatRows === 2
-                  ? "grid-cols-2"
-                  : "grid-cols-1"
-              } gap-x-4 text-textGrey`}
-            >
-              {rightRows.map((row) => (
-                <div
-                  key={row}
-                  className="w-12 h-12 flex items-center justify-center font-bold"
-                >
-                  {row}
-                </div>
-              ))}
-            </div>
-            <div
-              className={`grid ${
-                seatRows === 3
-                  ? "grid-cols-3"
-                  : seatRows === 2
-                  ? "grid-cols-2"
-                  : "grid-cols-1"
-              } gap-x-4 gap-y-3`}
-            >
-              {Array.from({ length: cols }, (_, i) => i + 1).map((col) =>
-                rightRows.map((row) => {
-                  const seat = `${row}${col}`;
-                  const isAvailable = !unavailableSeats.includes(seat);
-                  const label = getSeatLabel(seat);
-                  return (
-                    <button
-                      key={seat}
-                      onClick={() => isAvailable && toggleSeat(seat)}
-                      className={`w-12 h-12 flex items-center justify-center border rounded ${
-                        !isAvailable
-                          ? "bg-borderGrey cursor-not-allowed text-white"
-                          : label
-                          ? "bg-textPurple text-white"
-                          : "bg-lightGreen"
-                      }`}
-                      disabled={!isAvailable}
-                    >
-                      {isAvailable ? label || "" : "X"}
-                    </button>
-                  );
-                })
-              )}
-            </div>
+          <div className={`grid grid-cols-${leftRows.length} gap-x-4 gap-y-3`}>
+            {Array.from({ length: cols }, (_, i) => i + 1).map((col) =>
+              leftRows.map((row) => {
+                const seat = `${row}${col}`;
+                const isAvailable = !unavailableSeats.includes(seat);
+                const label = getSeatLabel(seat, isPergi);
+                return (
+                  <button
+                    key={seat}
+                    onClick={() => isAvailable && toggleSeat(seat, isPergi)}
+                    className={`w-12 h-12 flex items-center justify-center border rounded ${
+                      !isAvailable
+                        ? "bg-gray-400 cursor-not-allowed text-white"
+                        : label
+                        ? "bg-purple-600 text-white"
+                        : "bg-green-500"
+                    }`}
+                    disabled={!isAvailable}
+                  >
+                    {isAvailable ? label || "" : "X"}
+                  </button>
+                );
+              })
+            )}
           </div>
         </div>
+
+        <div className="flex flex-col items-center justify-center pt-12 gap-y-3">
+          {Array.from({ length: layout.cols }, (_, i) => i + 1).map((col) => (
+            <div
+              key={col}
+              className="flex items-center justify-center w-6 h-12 font-medium text-gray-600 border rounded-lg bg-gray-200"
+            >
+              {col}
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <div
+            className={`grid grid-cols-${rightRows.length} gap-x-4 text-gray-600`}
+          >
+            {rightRows.map((row) => (
+              <div
+                key={row}
+                className="flex items-center justify-center w-12 h-12 font-bold"
+              >
+                {row}
+              </div>
+            ))}
+          </div>
+          <div className={`grid grid-cols-${rightRows.length} gap-x-4 gap-y-3`}>
+            {Array.from({ length: cols }, (_, i) => i + 1).map((col) =>
+              rightRows.map((row) => {
+                const seat = `${row}${col}`;
+                const isAvailable = !unavailableSeats.includes(seat);
+                const label = getSeatLabel(seat, isPergi);
+                return (
+                  <button
+                    key={seat}
+                    onClick={() => isAvailable && toggleSeat(seat, isPergi)}
+                    className={`w-12 h-12 flex items-center justify-center border rounded ${
+                      !isAvailable
+                        ? "bg-gray-400 cursor-not-allowed text-white"
+                        : label
+                        ? "bg-purple-600 text-white"
+                        : "bg-green-500"
+                    }`}
+                    disabled={!isAvailable}
+                  >
+                    {isAvailable ? label || "" : "X"}
+                  </button>
+                );
+              })
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="w-full p-5 mx-auto">
+      <div className="p-5 border border-gray-300">
+        <h1 className="mb-2 text-xl font-semibold">Pilih Kursi</h1>
+
+        <Tabs value={activeTrip}>
+          <TabsHeader>
+            <Tab
+              key="Pergi"
+              value="Pergi"
+              onClick={() => setActiveTrip("Pergi")}
+            >
+              Pergi
+            </Tab>
+            <Tab
+              key="Pulang"
+              value="Pulang"
+              onClick={() => setActiveTrip("Pulang")}
+            >
+              Pulang
+            </Tab>
+          </TabsHeader>
+          <TabsBody>
+            <TabPanel key="Pergi" value="Pergi">
+              <div className="p-3 text-center rounded-md bg-green-500">
+                <p className="text-xs font-normal text-white">
+                  {seatClass} - {totalSeats} Seats available
+                </p>
+              </div>
+              {renderSeatMap(true)}
+            </TabPanel>
+            <TabPanel key="Pulang" value="Pulang">
+              <div className="p-3 text-center rounded-md bg-green-500">
+                <p className="text-xs font-normal text-white">
+                  {seatClass} - {totalSeats} Seats available
+                </p>
+              </div>
+              {renderSeatMap(false)}
+            </TabPanel>
+          </TabsBody>
+        </Tabs>
       </div>
     </div>
   );
