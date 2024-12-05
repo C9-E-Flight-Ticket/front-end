63b98492a415906043fdd53fc7bc88ea69a06890
unknown <almagiusunggane@gmail.com>
2024-12-05 08:55:59 +0700

    feat: add header order history

--- src/features/history/pages/HistoryPage.jsx
+++ src/features/history/pages/HistoryPage.jsx
@@ -1,9 +1,13 @@
 import MainLayout from "@/layouts/MainLayout";
 import TicketCard from "../components/TicketCard";
+import HistoryHeader from "../components/HistoryHeader";
 
 export default function HistoryPage() {
   return (
     <MainLayout>
+      <div className="w-full h-[260px] shadow-md">
+        <HistoryHeader />
+      </div>
       <div className="mt-48">
         <TicketCard />
       </div>
