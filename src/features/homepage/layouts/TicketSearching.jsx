bacf52f1daaab6da94fb3e52fa20b695e9e1bad9
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2024-12-05 22:43:14 +0800

    feat: add validation on searching button

--- src/features/homepage/layouts/TicketSearching.jsx
+++ src/features/homepage/layouts/TicketSearching.jsx
@@ -9,9 +9,11 @@ import {
 
 import MenuBodyContent from "@/features/homepage/layouts/MenuBodyContent";
 import { useNavigate } from "react-router-dom";
+import useCheckUserInput from "@/features/homepage/hooks/useCheckUserInput";
 
 export default function TicketSearching({ className }) {
   const navigate = useNavigate();
+  const isInputValid = useCheckUserInput();
 
   return (
     <Card
@@ -30,8 +32,9 @@ export default function TicketSearching({ className }) {
       <CardFooter className="py-0 px-0">
         <Button
           fullWidth
-          className="rounded-t-none bg-[#7126B5]"
+          className="rounded-t-none bg-[#7126B5] disabled:opacity-70"
           color="deep-purple"
+          disabled={!isInputValid}
           onClick={() => navigate("/flight")}
         >
           Cari Penerbangan
