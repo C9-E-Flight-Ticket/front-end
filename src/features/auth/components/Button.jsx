adbaf89859d0c14dc1106b62cb07b366667be295
Zainal <zainalfadliansyah@gmail.com>
2024-12-06 23:00:05 +0700

    refactor: simplify Button component with added onClick and flexible className props

--- src/features/auth/components/Button.jsx
+++ src/features/auth/components/Button.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-
-const Button = ({ type, text, color }) => {
+const Button = ({ text, color, onClick, className }) => {
   return (
     <button
-      type={type}
-      className={`w-full py-2 px-4 ${color} text-white font-semibold rounded-md hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75`}
+      onClick={onClick}
+      className={`${color} text-white py-2 px-4 rounded-lg ${className}`}
     >
       {text}
     </button>
