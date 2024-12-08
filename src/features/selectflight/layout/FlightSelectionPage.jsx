6f077d3c2727cd4c1a77896d23c6e6e57c1cc117
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2024-12-08 22:46:55 +0800

    fix: filterCard is struck down by selectedOrderCard when user order with a return flight
    TODO: responsive at selectedOrderCard

--- src/features/selectflight/layout/FlightSelectionPage.jsx
+++ src/features/selectflight/layout/FlightSelectionPage.jsx
@@ -1,19 +1,25 @@
-import MainLayout from "@/layouts/MainLayout";
 import Sorting from "../components/Sorting";
 import FilterCard from "../components/FilterCard";
 import AccordionFlight from "../components/AccordionFlight";
 import TicketSold from "../components/TicketSold";
 import FlightEmpty from "../components/FlightEmpty";
 import LoadingTicket from "../components/LoadingTicket";
+import { useSelector } from "react-redux";
 
 export default function FlightSelectionPage() {
+  const { stage } = useSelector((state) => state.flight);
+
   return (
     <>
       <Sorting />
       <div className="relative top-[320px] flex w-[968px] mx-auto">
         {/* <TicketSold className="mt-10 mx-auto" /> */}
 
-        <div className="fixed top-[390px] pt-4 -translate-x-[15%] w-[250px]">
+        <div
+          className={`fixed pt-4 -translate-x-[15%] w-[250px] ${
+            stage === "departure" ? "top-[390px]" : "top-[315px]"
+          }`}
+        >
           <FilterCard />
         </div>
 
