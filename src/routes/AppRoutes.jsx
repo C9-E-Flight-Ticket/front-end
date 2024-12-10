cec33504c06273bd2dfb210d1caae61cd49033a5
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2024-12-10 12:27:09 +0800

    fix: select file importing bug

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Homepage from "@/features/homepage/pages/Homepage";
 import NotFound from "@/components/NotFound";
-import SelectFlight from "@/features/selectFlight/pages/SelectFlight";
 import Transaction from "@/features/transaction/pages/TransactionPage";
 import Payment from "@/features/transaction/pages/PaymentPage";
 import PaymentSuccessPage from "@/features/transaction/pages/PaymentSuccessPage";
@@ -13,6 +12,7 @@ import AccountPage from "@/features/account/pages/AccountPage";
 import Login from "@/features/auth/pages/Login";
 import ProtectedRoute from "@/utils/ProtectedRoute";
 import ResetPassword from "@/features/auth/pages/resetPassword";
+import SelectFlight from "@/features/selectFlight/pages/SelectFlight";
 
 const routes = [
   {
