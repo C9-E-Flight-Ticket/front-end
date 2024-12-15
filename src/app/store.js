37a020c8b0c798705537ca014ba15f0cdfdca984
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2024-12-15 10:06:29 +0800

    feat: add forgot password api service

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { flightApi } from "@/services/api/flightApi";
 import { authApi } from "@/services/api/authApi";
 import { transactionApi } from "@/services/api/transactionApi";
+import { accountApi } from "@/services/api/accountApi";
 
 const store = configureStore({
   reducer: {
@@ -12,13 +13,15 @@ const store = configureStore({
     [flightApi.reducerPath]: flightApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [transactionApi.reducerPath]: transactionApi.reducer,
+    [accountApi.reducerPath]: accountApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(flightApi.middleware)
       .concat(authApi.middleware)
-      .concat(transactionApi.middleware),
+      .concat(transactionApi.middleware)
+      .concat(accountApi.middleware),
 });
 
 export default store;
