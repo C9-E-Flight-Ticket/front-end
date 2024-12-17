502abf547537ec02d4396a225a6b8c0b3ade9172
RifqiAfandi <rifqitriafandi.20@gmail.com>
2024-12-17 15:20:53 +0700

    refactor: size header menu

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -10,7 +10,7 @@ const HeaderMenu = ({ title, headerId }) => {
       <div className="fixed lg:top-[106px] md:top-[106px] top-[86px] lg:left-1/2 left-4 lg:-translate-x-[490px] lg:text-xl md:text-xl text-base font-bold">
         {title}
       </div>
-      <div className="fixed lg:top-[150px] md:top-[150px] top-[105px] lg:left-1/2 lg:-translate-x-1/2 lg:w-[968px] md:w-[900px] w-[330px] py-2 px-4 flex items-center lg:gap-3 md:gap-3 gap:2">
+      <div className="fixed lg:top-[150px] md:top-[150px] top-[105px] lg:left-1/2 lg:-translate-x-1/2 lg:w-[968px] md:w-[750px] w-[330px] py-2 px-4 flex items-center lg:gap-3 md:gap-3 gap:2">
         <div
           className={`lg:h-[50px] md:h-[50px] h-[35px] flex items-center gap-2 bg-[#A06ECE] py-[5px] px-4 rounded-xl text-white ${
             headerId === 1 ? "w-[777px]" : "w-[936px]"
