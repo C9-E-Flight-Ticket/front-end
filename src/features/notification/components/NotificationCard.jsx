1266f9188052ce59bad7d31b4d9f0568f5aaa3f0
RifqiAfandi <rifqitriafandi.20@gmail.com>
2024-12-18 18:13:31 +0700

    fix: responsive notification for relative size

--- src/features/notification/components/NotificationCard.jsx
+++ src/features/notification/components/NotificationCard.jsx
@@ -8,9 +8,9 @@ const NotificationCard = ({
 }) => {
   return (
     <a href={navigasi}>
-      <div className="lg:w-[780px] md:w-[480px] w-[280px] flex items-start gap-4">
+      <div className="lg:w-[780px] md:w-[600px] sm:w-[440px] w-[280px] flex items-start gap-4">
         <img src="/circle-notification.svg" />
-        <div className="lg:w-[740px] md:w-[440px] w-[240px] grid gap-[4px]">
+        <div className="lg:w-[740px] md:w-[560px] sm:w-[400px] w-[240px] grid gap-[4px]">
           <div className="flex justify-between lg:text-sm md:text-[14px] text-[12px] leading-[14px] font-normal text-[#8A8A8A]">
             {title}
             <div className="flex gap-2">
